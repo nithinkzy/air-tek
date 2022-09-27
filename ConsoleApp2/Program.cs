e9a00f4091ac8cf09c8a39fb37747c89cbbd129f
Nithin Kumar K S <nithinkzy@gmail.com>
2022-09-26 22:56:40 -0400

    output first test case

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,59 +1,15 @@
-﻿
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
-class TestClass
+class Transport
 {
-    private List<Orders> generateflightitinery(Dictionary<string, Dictionary<string, string>>? json, Dictionary<int, List<Flight>> flightschedule)
-    {
-        List<Orders> fi = new List<Orders>();
-        foreach (var order_id in json.Keys)
-        {
-            for (int i = 1; i <= flightschedule.Count; i++)
-            {
-                //string sdsdsd = json[order_id]["destination"];
-                Flight reqflight = new Flight();
-                reqflight = flightschedule[i].Find(x => x.arrival == json[order_id]["destination"]);
-                if (reqflight != null)
-                {
-                    if (json[order_id]["destination"] == reqflight.arrival)
-                    {
-                        if (reqflight.cargo < reqflight.capacity)
-                        {
-                            reqflight.cargo++;
-                            fi.Add(new Orders { flight = reqflight, ID = order_id, day = i });
-                            json.Remove(order_id);
-                            break;
-                        }
-                        else
-                        {
-                            continue;
-                        }
-                    }
-                }
-                else
-                {
-                    fi.Add(new Orders { flight = null, ID = order_id, day = i });
-                    json.Remove(order_id);
-                    break;
-                }
-
-            }
-        }
-        return fi;
-    }
     class Flight
     {
-
-        public string departure { get; set; }
-        //public string orderId { get; set; }
-        public string arrival { get; set; }
-
-        //public int day { get; set; }
         public int number { get; set; }
+        public string departure { get; set; }
+        public string arrival { get; set; }
         public int capacity { get; set; }
         public int cargo { get; set; }
     }
-
 
     class Orders
     {
@@ -61,9 +17,45 @@ class TestClass
         public Flight flight { get; set; }
         public int day { get; set; }
     }
+
+    private List<Orders> generateflightitinery(Dictionary<string, Dictionary<string, string>>? json, Dictionary<int, List<Flight>> flightschedule)
+    {
+        List<Orders> flightItineray = new List<Orders>();
+        foreach (var order_id in json.Keys)
+        {
+            for (int day = 1; day <= flightschedule.Count; day++)
+            {
+                Flight flight = new Flight();
+                flight = flightschedule[day].Find(x => x.arrival == json[order_id]["destination"]);
+                if (flight != null)
+                {
+                    if (flight.capacity < flight.cargo)
+                    {
+                        continue;
+                    }
+                    flight.cargo++;
+                    flightItineray.Add(new Orders { flight = flight, ID = order_id, day = day });
+                    json.Remove(order_id);
+                    break;
+                }
+                else
+                {
+                    flightItineray.Add(new Orders { flight = null, ID = order_id, day = day });
+                    json.Remove(order_id);
+                    break;
+                }
+
+            }
+        }
+        return flightItineray;
+    }
+
     static void Main(string[] args)
     {
-
+        /*
+           USER STORY 1
+           GENERATE FLIGHT ININERARIES
+        */
         Dictionary<int, List<Flight>> flightschedule = new Dictionary<int, List<Flight>>();
         flightschedule.Add(1, new List<Flight>()
         {
@@ -79,19 +71,36 @@ class TestClass
                 new Flight(){  arrival="YVR", departure="YUL", number =6  , capacity=20, cargo=0},
 
         });
+
+
+        foreach (int day in flightschedule.Keys)
+        {
+            var flights = flightschedule[day];
+            foreach (var flight in flights)
+            {
+                Console.WriteLine("Flight: {0}, departure: {1}, arrival: {2}, day: {3}", flight.number, flight.departure, flight.arrival, day);
+                Console.WriteLine("");
+            }
+        }
+
+
+        /*
+            USER STORY 2
+            GENERATE FLIGHT ININERARIES
+         */
         var json = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText("N:\\Code\\Code\\C#\\app\\coding-assigment-orders.json"));
         List<Orders> orders = new List<Orders>();
-        TestClass cdd = new TestClass();
-        orders = cdd.generateflightitinery(json, flightschedule);
-        orders = (from oo in orders orderby oo.day select oo).ToList();
-        foreach (var c in orders)
+        Transport inventoryManagemenet = new Transport();
+        orders = inventoryManagemenet.generateflightitinery(json, flightschedule);
+
+        //Print desired output
+        orders = (from order in orders orderby order.day select order).ToList();
+        foreach (var order in orders)
         {
-            if (c.flight == null)
-                Console.WriteLine("order: {0}, flightNumber: not scheduled", c.ID);
+            if (order.flight == null)
+                Console.WriteLine("order: {0}, flightNumber: not scheduled", order.ID);
             else
-                Console.WriteLine("order: {0}, flightNumber: {1}, departure: {2}, arrival: {3}, day: {4}, cargo: {5}", c.ID, c.flight.number, c.flight.departure, c.flight.arrival, c.day, c.flight.cargo);
+                Console.WriteLine("order: {0}, flightNumber: {1}, departure: {2}, arrival: {3}, day: {4}, cargo: {5}", order.ID, order.flight.number, order.flight.departure, order.flight.arrival, order.day, order.flight.cargo);
         }
     }
-
-    
 }
